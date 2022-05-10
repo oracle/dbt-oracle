1358bbca52d615a1771cc66f6e6b36a2e7aa8a56
Thomas Sapelza <thomas.sapelza@gknpm.com>
2022-05-10 11:56:57 +0200

    Plz fix #3 Signed-off-by: Thomas Sapelza <sapelza.thomas@gmail.com>
    Signed-off-by: Thomas Sapelza <thomas.sapelza@gknpm.com>

--- dbt/adapters/oracle/connections.py
+++ dbt/adapters/oracle/connections.py
@@ -46,7 +46,7 @@ class OracleConnectionMethod(enum.Enum):
     CONNECTION_STRING = 3
 
 
-@dataclass(init=False)
+@dataclass
 class OracleAdapterCredentials(Credentials):
     """Collect Oracle credentials
 
@@ -83,11 +83,6 @@ class OracleAdapterCredentials(Credentials):
     _ALIASES = {
         'pass': 'password',
     }
-
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-            self.database = None
 
     def __post_init__(self):
         # In Oracle the userenv DB_NAME (database) is not needed when initiating a connection
