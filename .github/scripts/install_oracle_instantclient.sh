e178e6780165280cbee5b329e72a867ee30565d2
Abhishek Singh <abhishek.o.singh@oracle.com>
2022-05-17 12:33:01 -0700

    Fix: packets received failed

--- .github/scripts/install_oracle_instantclient.sh
+++ .github/scripts/install_oracle_instantclient.sh
@@ -5,6 +5,6 @@ wget https://download.oracle.com/otn_software/linux/instantclient/216000/instant
 sudo unzip /tmp/instantclient-basiclite-linux.x64-21.6.0.0.0dbru.zip -d /opt/oracle
 export PATH="$PATH:/opt/oracle/instantclient_21_6"
 export LD_LIBRARY_PATH="/opt/oracle/instantclient_21_6:$LD_LIBRARY_PATH"
-sudo mkdir -p /opt/oracle
+sudo mkdir -p /opt/tns_admin
 echo "DISABLE_OOB=ON" >> /opt/tns_admin/sqlnet.ora
 
