d87c20f852013ffd6a65a94eb62df9fbfb84ce51
Abhishek Singh <abhishek.o.singh@oracle.com>
2023-06-12 21:37:11 -0700

    Upgraded version to 1.5.1

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Configuration variables
-VERSION=1.5.0
+VERSION=1.5.1
 PROJ_DIR?=$(shell pwd)
 VENV_DIR?=${PROJ_DIR}/.bldenv
 BUILD_DIR=${PROJ_DIR}/build
--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ project_urls = {
 
 url = 'https://github.com/oracle/dbt-oracle'
 
-VERSION = '1.5.0'
+VERSION = '1.5.1'
 setup(
     author="Oracle",
     python_requires='>=3.7.2',
