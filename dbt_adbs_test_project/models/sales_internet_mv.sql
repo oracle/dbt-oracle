42687955cf692a4b8477f305b99d5cb9a0ab7176
Abhishek Singh <abhishek.o.singh@oracle.com>
2024-07-01 14:57:19 -0700

    Fix ORA-12098: cannot comment on the materialized view

--- dbt_adbs_test_project/models/sales_internet_mv.sql
+++ dbt_adbs_test_project/models/sales_internet_mv.sql
@@ -13,6 +13,6 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 #}
-{{ config(materialized='materialized_view')}}
+{{ config(materialized='materialized_view',  persist_docs={"relation": true, "columns": true}) }}
 select * from {{ source('sh_database', 'sales') }}
 where channel_id = 5
