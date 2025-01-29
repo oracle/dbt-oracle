4b762cf98ce554533816dc47eaacd25f90a4e4f0
Abhishek Singh <abhishek.o.singh@oracle.com>
2025-01-29 14:54:01 -0800

    Added link to libaio.so.1 needed for Oracle client libraries

--- .github/scripts/install_oracle_instantclient.sh
+++ .github/scripts/install_oracle_instantclient.sh
@@ -5,6 +5,7 @@ wget https://download.oracle.com/otn_software/linux/instantclient/2370000/instan
 sudo unzip /tmp/instantclient-basiclite-linux.x64-23.7.0.25.01.zip -d /opt/oracle
 export PATH="$PATH:/opt/oracle/instantclient_23_7"
 export LD_LIBRARY_PATH="/opt/oracle/instantclient_23_7:$LD_LIBRARY_PATH"
+sudo ln -s /usr/lib/x86_64-linux-gnu/libaio.so.1t64 /usr/lib/x86_64-linux-gnu/libaio.so.1
 sudo mkdir -p /opt/tns_admin
 echo "DISABLE_OOB=ON" >> /opt/tns_admin/sqlnet.ora
 
